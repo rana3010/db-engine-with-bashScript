8bdc1741c37f074e764be82a70c0f27cc2f3bf9f
rana <ranawael9721@gmail.com>
2022-01-16 00:13:35 +0200

    Adding insert table script

--- insertTB.sh
+++ insertTB.sh
@@ -4,22 +4,70 @@ if [  $table == "back" ]
 then
 	clear 
 	. selectTable.sh
-fi
-
-if [[ -f "$table" ]]
-then
-	row=`head -n1 $table`
-	
 else
-	val1="Re-enter name of table"
-	val2="Go back to select table MENU"
-	echo "There is no table with this name!"
-	select val in "$val1" "$val2"
-	do
-		case $val in
-			$val1 ) clear ; . insertTB.sh.sh ; clear ; break;;
-			$val2 ) clear ; . selectTable.sh ; clear ; break;;
-			* ) echo "Invalid choice"
-		esac
-	done
+	if [[ -f "$table" ]]
+	then
+		NF=$(awk -F : 'END{print NR}' $table)
+		((id = $NF - 2 )) 
+		row="$id:"
+		field=$(awk -F : 'END{print NF}' $table)
+		for (( i = 2; i <= $field ; i++ )) 
+		do
+			name=$(awk -F : 'BEGIN {field = '$i'}{if(NR==1){print $field;}}' $table)
+			datatype=$(awk -F : 'BEGIN {field = '$i'}{if(NR==2){print $field;}}' $table)
+			echo "Enter the value of field ($name):"
+			read val
+			if [[ $datatype == *"Not NULL"* ]]
+			then
+				while  [[ $val == "" ]] 
+				do
+					echo  "This field must not be empty!"
+					read val
+				done			
+			fi
+			if [[ $datatype == *"int"* ]]
+			then
+				while ! [[ $val =~ ^[0-9]*$ ]] 
+				do
+					echo  "Invalid datatype!"
+					read val
+					while  [[ $val == "" ]] 
+					do
+						echo  "This field must not be empty!"
+						read val
+					done
+				done
+			fi
+			if [[ i -eq $field ]]
+			then
+				row+="$val"
+			else
+				row+="$val:"
+			fi
+		done
+		echo $row>>$table
+		echo "The record is inserted to $table successfully :)"
+		echo "Insert another record?"
+		select check in "Yes" "No"
+		do
+			case $check in
+				"Yes" ) clear ; . insertTB.sh    ; clear ; break;;
+				"No" )  clear ; . selectTable.sh ; clear ; break;;
+				* ) echo "Invalid choice";
+			esac
+		done
+		
+	else
+		echo "There is no table with this name!"
+		val1="Re-enter name of table"
+		val2="Go back to select table MENU"
+		select val in "$val1" "$val2"
+		do
+			case $val in
+				$val1 ) clear ; . insertTB.sh    ; clear ; break;;
+				$val2 ) clear ; . selectTable.sh ; clear ; break;;
+				* ) echo "Invalid choice"
+			esac
+		done
+	fi
 fi
