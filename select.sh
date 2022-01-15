6a9b9e15a2e5e1291fbab10eb6e932880af717b3
NourhanIbraheem <nouraibraheem82@gmail.com>
2022-01-15 05:16:00 +0200

    Edit at select.sh

--- select.sh
+++ select.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
-
-echo "Please enter your choice: "
+echo "~~~ MENU ~~~"
+echo "-Please Enter The Number Of Choice: "
 options=("Create DataBase" "List" "Connect To DB" "Drop DB" "Quit")
 select val in "${options[@]}"
 do
     case $val in
         "Create DataBase")
-           clear ; . createdb.sh ; select.sh
+           clear ; . createdb.sh ; . backTOmenu.sh
             ;;
         "List")
-           clear ; . listdb.sh ; select.sh
+           clear ; . listdb.sh ; . backTOmenu.sh
             ;;
         "Connect To DB")
-            clear ; . connect.sh ; select.sh
+            clear ; . connect.sh ; . backTOmenu.sh
             ;;
         "Drop DB")
-            clear ; . drop.sh ; select.sh
+            clear ; . drop.sh ; . backTOmenu.sh
             ;;  
         "Quit")
-            clear ; break
+            clear ; exit
             ;;
-        *) clear ; echo "invalid option $REPLY" ; select.sh ;;
+        *) clear ; echo "invalid option $REPLY" ; . select.sh ;;
     esac
 done
